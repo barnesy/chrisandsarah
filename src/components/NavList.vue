168d3a7e2a52fdcefa79b145b506c6df1e4762e3
precious <chris@barnesy.me>
2022-07-31 17:11:20 -0400

    Removes escapes

--- src/components/NavList.vue
+++ src/components/NavList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <li><g-link class="nav__link" to="https://www.honeyfund.com/site/barnes-ruiz-10-01-2022">We\'re not registered</g-link></li>
+    <li><g-link class="nav__link" to="https://www.honeyfund.com/site/barnes-ruiz-10-01-2022">We're not registered</g-link></li>
     <li><g-link class="nav__link" to="/map/">Map</g-link></li>
     <li><g-link class="nav__link" to="/rsvp/">RSVP</g-link></li>
   </ul>
